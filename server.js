98f905e5078430f5cef27f83f21697b5896c630b
omar-gaal <omargaal123@gmail.com>
2025-08-21 17:46:18 +0200

    adding port and, add type for pj

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Hello Node!");
+import http from "node:http";
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server !");
+});
+
+server.listen(PORT, () => console.log(`server running on, ${PORT}`));
+
+// https://scrimba.com/learn-nodejs-c00ho9qqh6/~07du
